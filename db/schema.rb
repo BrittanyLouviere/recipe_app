1942e2ff490243a3d68f4328673ada14fa398ca6
BrittanyLouviere <brittanynlouviere.gmail.com>
2020-12-19 18:36:52 -0600

    Add recipe scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_19_193251) do
+ActiveRecord::Schema.define(version: 2020_12_20_000825) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["name"], name: "index_ingredients_on_name", unique: true
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.text "instructions"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
